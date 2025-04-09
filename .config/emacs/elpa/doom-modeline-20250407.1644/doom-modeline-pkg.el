474abee919d410673b291b0a983701db6b1e5dc1
wisearid <cbryson2201@gmail.com>
2025-04-09 18:53:24 -0400

    commit message

---
+++ .config/emacs/elpa/doom-modeline-20250407.1644/doom-modeline-pkg.el
@@ -0,0 +1,13 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "doom-modeline" "20250407.1644"
+  "A minimal and modern mode-line."
+  '((emacs       "25.1")
+    (compat      "29.1.4.5")
+    (nerd-icons  "0.1.0")
+    (shrink-path "0.3.1"))
+  :url "https://github.com/seagle0128/doom-modeline"
+  :commit "c59909587760e81aebba25d4490914fc25266ac8"
+  :revdesc "c59909587760"
+  :keywords '("faces" "mode-line")
+  :authors '(("Vincent Zhang" . "seagle0128@gmail.com"))
+  :maintainers '(("Vincent Zhang" . "seagle0128@gmail.com")))
---
+++ .config/emacs/init.el
@@ -0,0 +1,90 @@
+;; init.el
+
+;; Set up package sources
+(require 'package)
+(setq package-enable-at-startup nil) ; let use-package handle packages
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+;; Bootstrap use-package
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+;; Speed up startup
+(setq gc-cons-threshold (* 50 1000 1000)) ; 50MB
+(setq read-process-output-max (* 1024 1024)) ;; 1MB
+
+;; UI Tweaks - Doom-like
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(setq inhibit-startup-screen t)
+(set-fringe-mode 10)
+(setq visible-bell t)
+(global-display-line-numbers-mode t)
+(column-number-mode)
+
+;; Font
+(set-face-attribute 'default nil :font "Hack" :height 120)
+
+;; Theme - Dracula
+(use-package dracula-theme
+  :config
+  (load-theme 'dracula t))
+
+;; Doom-like Modeline
+(use-package doom-modeline
+  :hook (after-init . doom-modeline-mode)
+  :custom
+  (doom-modeline-height 15))
+
+;; Icons (for modeline, etc)
+(use-package all-the-icons
+  :if (display-graphic-p))
+
+;; Projectile for project management
+(use-package projectile
+  :init
+  (projectile-mode +1)
+  :custom ((projectile-completion-system 'auto))
+  :bind-keymap
+  ("C-c p" . projectile-command-map))
+
+;; Ivy for completion (Doom uses vertico now, but Ivy is snappy and similar)
+(use-package ivy
+  :diminish
+  :config
+  (ivy-mode 1))
+
+;; Which-key (Doom has it too)
+(use-package which-key
+  :defer 0
+  :diminish
+  :config
+  (which-key-mode)
+  (setq which-key-idle-delay 0.3))
+
+(use-package dashboard
+  :ensure t
+  :config
+  (dashboard-setup-startup-hook))
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(require 'dashboard)
+(dashboard-setup-startup-hook)
