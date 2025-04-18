b3b1d33eef7605d9a7a7251cebffc725deb2ccf9
wisearid <cbryson2201@gmail.com>
2025-04-17 21:34:39 -0400

    stuff

--- st/config.def.h
+++ st/config.def.h
@@ -5,10 +5,10 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "monospace:size=12:antialias=true:autohint=true";
+static char *font = "Hack Nerd Font:size=13:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = { 
-	"Ubuntu Mono:size=12:antialias=true:autohint=true",
+	"Hack Nerd Font:size=13:antialias=true:autohint=true",
 };
 
 static int borderpx = 2;
